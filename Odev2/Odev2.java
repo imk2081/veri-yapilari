364ff9e7dfb53dc106345865dccdde3cd32dfd8e
imk2081 <44306318+imk2081@users.noreply.github.com>
2018-12-01 07:42:46 -0800

    Update Odev2.java

--- Odev2/Odev2.java
+++ Odev2/Odev2.java
@@ -7,141 +7,134 @@ public class Odev2 {
 
     //girdi ve bos agac gonderiliryor
     public static String huffmanKodla(String girdi, HuffmanTree agac) {
-                String[] words = girdi.split("");
-                HashMap<String, Integer> hm = new HashMap<String, Integer>(); 
-                for (String a : words) {
-                Integer freq = hm.get(a);
-                hm.put(a, (freq == null) ? 1 : freq + 1);
-                } 
-                HuffmanNode[] hArray = new HuffmanNode[hm.size()];
-                int index = 0;
-                //>>>>>>>>>>>>>>
-                for (Object key : hm.keySet()) {
-                    int frequency = hm.get(key);
-                    String MyKeyInString = key.toString();
-                
-                    char MyKeyInChar = 'X';
-                    if (MyKeyInString == "") {
-                        return null;
-                    }
-                    else
-                    {
-                        MyKeyInChar = MyKeyInString.charAt(0);
-                    }
-                    HuffmanNode MYNode = new HuffmanNode(MyKeyInChar,frequency,null,null);
-                    hArray[index] = MYNode;
-                    index++;
+            String[] words = girdi.split("");
+            HashMap<String, Integer> hm = new HashMap<String, Integer>(); 
+            for (String a : words) {
+            Integer freq = hm.get(a);
+            hm.put(a, (freq == null) ? 1 : freq + 1);
+            } 
+            HuffmanNode[] hArray = new HuffmanNode[hm.size()];
+            int index = 0;
+            //>>>>>>>>>>>>>>
+            for (Object key : hm.keySet()) {
+                int frequency = hm.get(key);
+                String MyKeyInString = key.toString();
+            
+                char MyKeyInChar = 'X';
+                if (MyKeyInString == "") {
+                    return null;
                 }
-                //^^^^^^^^^^^^^^
-                agac.addAll(hArray);
-                HashMap myMap =new HashMap();
-                /**/
-                myMap = findPath(agac.root);
-                /**/
-                Object[] myKeys = myMap.keySet().toArray();
-                Object[] myValues = myMap.values().toArray();
-                String myString = "";
-                int i = 0;
-                int j = 0;
-                //>>>>>>>>>>>>>>
-                for (String a: words) {
-                    char c = a.charAt(0);
-                    for(Object cc: myKeys)
-                    {
-                        cc = c;
-                        if (myKeys[j].equals(c)) {
-                            myString = myString + myValues[j];
-                        }
-                        j++;
-                    }
-                    j=0;
-                    i++;
+                else
+                {
+                    MyKeyInChar = MyKeyInString.charAt(0);
                 }
-                //^^^^^^^^^^^^^^
+                HuffmanNode MYNode = new HuffmanNode(MyKeyInChar,frequency,null,null);
+                hArray[index] = MYNode;
+                index++;
+            }
+            //^^^^^^^^^^^^^^
+            agac.addAll(hArray);
+            HashMap myMap =new HashMap();
+            /**/
+            myMap = findPath(agac.root);
+            /**/
+            Object[] myKeys = myMap.keySet().toArray();
+            Object[] myValues = myMap.values().toArray();
+            String myString = "";
+            int i = 0;
+            int j = 0;
+            //>>>>>>>>>>>>>>
+            for (String a: words) {
+                char c = a.charAt(0);
+                for(Object cc: myKeys)
+                {
+                    cc = c;
+                    if (myKeys[j].equals(c)) {
+                        myString = myString + myValues[j];
+                    }
+                    j++;
+                }
+                j=0;
+                i++;
+            }
+            //^^^^^^^^^^^^^^
         agac.addAll(hArray);
         return myString;
-    }
+    }//huffmanKodla
+    
+    
+    //yardimci methods
     
     static public HashMap findPath (HuffmanNode root){
     HashMap map = new HashMap();
     HuffmanNode current = root;
     String path = "";
     while(true){
-        //yaprak ise
-        if (isLeaf(current)) {
-            map.put(current.value, path);
-            if (path.length() > 0) {
-                path = sil(path);
-            }
-            current = findParent(current, root);
-            if (current == null) {
-                    //kok e geri geldimi gelme
-                return map;
-            }
-            //map da var ise node sil
-            if (current.left != null) {
-                    //map da var ise sil
-                    if (map.get(current.left.value) != null) {
-                    current.left=null;
-                }
-            }
-            if (current.right != null) {
-                    //map da var ise sil
-                    if (map.get(current.right.value) != null) {
-                    current.right=null;
-                }
-            }
-            //kok e geri geldimi gelme
-            else{
-                return map;
-            }
-        }
-        //if not ok current = null
-        else if (LeftOk(current)) {
-            path = path + "0";
-            current = goLeft(current);
-            continue;
-        }
-        else if (RightOk(current)) {
-            path = path + "1";
-            current = goRight(current);
-            continue;
-        }
-        //ustte gelme
-        else{
-            if (!isLeaf(current)) {
+            //yaprak ise
+            if (isLeaf(current)) {
+                map.put(current.value, path);
                 if (path.length() > 0) {
                     path = sil(path);
                 }
                 current = findParent(current, root);
+                //current node kok e geri geldimi kontrolu
                 if (current == null) {
-                    //kok e geri geldimi gelme
                     return map;
                 }
                 if (current.left != null) {
-                    if (!isLeaf(current.left)) {
-                    current.left = null;
+                        //deger HashMap de var ise node sil
+                        if (map.get(current.left.value) != null) {
+                        current.left=null;
                     }
                 }
                 if (current.right != null) {
-                    if (!isLeaf(current.right) && !isNode(current.right)) {
-                    current.right = null;
+                        //deger HashMap de var ise node sil
+                        if (map.get(current.right.value) != null) {
+                        current.right=null;
+                    }
+                }
+                //current node kok e geri geldimi kontrolu
+                else{
+                    return map;
+                }
+            }
+            //if not ok current = null
+            else if (LeftOk(current)) {
+                path = path + "0";
+                current = goLeft(current);
+                continue;
+            }
+            else if (RightOk(current)) {
+                path = path + "1";
+                current = goRight(current);
+                continue;
+            }
+            //to the parent
+            else{
+                if (!isLeaf(current)) {
+                    if (path.length() > 0) {
+                        path = sil(path);
+                    }
+                    current = findParent(current, root);
+                    //current node kok e geri geldimi kontrolu
+                    if (current == null) {
+                        return map;
+                    }
+                    if (current.left != null) {
+                        if (!isLeaf(current.left)) {
+                        current.left = null;
+                        }
+                    }
+                    if (current.right != null) {
+                        if (!isLeaf(current.right) && !isNode(current.right)) {
+                        current.right = null;
+                        }
                     }
                 }
             }
-        }
-    }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    } 
+        }//while
+    }//findPath
+    
     static public boolean isNode(HuffmanNode node){
         if (node.value == null && node.left == null && node.right == null) {
             return false;
@@ -219,4 +212,4 @@ public class Odev2 {
         a=bulid.toString(); 
         return a;
     }
-}
+}//Odev2
